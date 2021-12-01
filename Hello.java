1caf566af584f722872d8329004ec1b4ffceacdb
phvcg <95334455+phvcg@users.noreply.github.com>
2021-12-01 22:40:20 +1100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
   public static void main(String [] args) {
     // displays "Hello Java" in console
-    System.out.println("Hell Java");
+    System.out.println("Hell Test");
     }
 }
